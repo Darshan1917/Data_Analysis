332bbcec4fb5ab4e1197eaf64b3f3550c90f1461
Darshan1917 <darshankobe710@gmail.com>
2018-02-20 14:37:34 +0530

    linear regression

--- Assignemnt_1.py
+++ Assignemnt_1.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from matplotlib import style
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 
 
@@ -18,12 +19,18 @@ value = data.isnull()
 print (data.isnull().sum())
 
 # what does inplace mean
-#print(data.dtypes)
+print(data.dtypes)
 data = data.drop(['CarName','car_ID'], axis=1)
-obj_df = data.select_dtypes(include=['object']).copy()
-obj_df1 = data.select_dtypes(exclude=['object']).copy()
-print (obj_df1.head())
-
+#data = data.select_dtypes(include=['object']).copy()
+obj_df1 = data.select_dtypes(include=['object']).copy()
+#print (np.unique(obj_df1['aspiration']))
+#print (pd.get_dummies(obj_df1['aspiration']))
+# when we use get dummies the values gets over written if we assign it to same value
+print ("before:" ,obj_df1.head())
+print("####################")
+obj_df1 = pd.get_dummies(obj_df1)
+#print (obj_df1['doornumber']) # uncomment and check error
+print ("After", obj_df1.head())
 #  
 #fueltype_mapping = {'gas':1,'diesel':0}
 #data['fueltype'] =  data['fueltype'].map(fueltype_mapping)
@@ -40,9 +47,43 @@ print (obj_df1.head())
 #print(np.unique(obj_df['enginetype']))
 #
 ##type one 
-data_dummies = pd.get_dummies(obj_df)
+data_dummies = pd.get_dummies(data , drop_first=True)
 print (data_dummies.head())
-features = data_dummies
+X = np.array(data_dummies.drop(['price'],1))
+X = preprocessing.scale(X)
+Y = np.array(data_dummies['price'])
+
+
+X_train , X_test ,Y_train , Y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
+print ('X_train:' , X_train.shape)
+print ('X_test' , X_test.shape)
+print ('Y_train' , Y_train.shape)
+print ('Y_test' , Y_test.shape)
+clf = LinearRegression()
+clf.fit(X_train , Y_train)
+Y_pred = clf.predict(X_test)
+#print (Y_pred)
+print("Y_pred" , Y_pred[0])
+print ("Y_test" , Y_test[0])
+#print('Coefficients: \n', clf.coef_)
+print("Mean squared error: %.2f"
+      % mean_squared_error(Y_test, Y_pred))
+print('Variance score: %.2f' % r2_score(Y_test, Y_pred))
+m = clf.coef_[0]
+b = clf.intercept_
+print ("formula y= {0} x + {1} :".format(m,b))
+
+#plt.scatter(X_test, Y_test,  color='black')
+#plt.plot(X_test, Y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+accuracy = clf.score(X_test,Y_test)
+print (accuracy)
+
+
 
 # methond 2
 #label_encoder = LabelEncoder()
@@ -62,9 +103,6 @@ features = data_dummies
 #integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
 #onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
 #print(onehot_encoded)
-Frame = [obj_df1,features]
-X = pd.concat(Frame)
-print (X.head())
 
 
 
---
+++ testing.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 13:12:32 2018
+
+@author: darshan
+"""
+
+import pandas as pd
+
+df = pd.DataFrame
